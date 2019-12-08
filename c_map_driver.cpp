42d55f047e1795d09e64264ad668487c32753901
Saadiq Daniels <s.daniels@digipen.edu>
2019-12-07 19:53:48 -0800

    Adjusted formatting in drivers

--- c_map_driver.cpp
+++ c_map_driver.cpp
@@ -22,5 +22,6 @@
  */
 int main()
 {
-    return 0;
+  
+  return 0;
 }
--- s_vector_driver.cpp
+++ s_vector_driver.cpp
@@ -24,33 +24,40 @@
  */
 int main()
 {
-    // Three different data structures
-    std::list<derived1> list_d1;
-    std::vector<derived1> vector_d1;
-    std::list<derived3> list_d3;
-
-    // Push data back into all of the containers
-    for(int i = 0; i < 10; ++i)
+  // Three different data structures
+  std::list<derived1>   list_d1;
+  std::vector<derived1> vector_d1;
+  std::list<derived3>   list_d3;
+  
+  // Push data back into all of the containers
+  for (int i = 0; i < 10; ++i)
+  {
+    // Push to the front of the list
+    list_d1.push_front(i);
+    // Push to the back of the vector
+    vector_d1.push_back(i);
+    
+    // Alternate between pushing to the front and back of the list
+    if (i % 2)
     {
-        // Push to the front of the list
-        list_d1.push_front(i);
-        // Push to the back of the vector
-        vector_d1.push_back(i);
-
-        // Alternate between pushing to the front and back of the list
-        if(i % 2)
-            list_d3.push_front(derived3());
-        else
-            list_d3.emplace_back(derived3());
+      list_d3.push_front(derived3());
     }
-
-    assert(list_d1.size() == vector_d1.size() && vector_d1.size() == list_d3.size());
-
-    it_rtr list_d1_it_rtr = MakeIterator<base>(list_d1.begin());
-    cit_rtr list_d1_cit_rtr = MakeCIterator<base>(list_d1.end());
-
-    // Print the d1 list using the iterators
-    helper::print(list_d1_it_rtr, list_d1_cit_rtr);
-
-    return 0;
+    else
+    {
+      list_d3.emplace_back(derived3());
+    }
+  }
+  
+  assert(list_d1.size() == vector_d1.size() && vector_d1.size() == list_d3.size());
+  
+  it_rtr  list_d1_it_rtr  = MakeIterator<base>(list_d1.begin());
+  cit_rtr list_d1_cit_rtr = MakeCIterator<base>(list_d1.end());
+  
+  // Print the d1 list using the iterators
+  helper::print(list_d1_it_rtr, list_d1_cit_rtr);
+  
+  delete *list_d1_it_rtr;
+  delete *list_d1_cit_rtr;
+  
+  return 0;
 }
